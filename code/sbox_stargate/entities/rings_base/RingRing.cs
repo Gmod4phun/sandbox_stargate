2fbafa3c64f562a3546c0a50ea182aff1c0cd283
Mathis Cucci <mat.spyder@gmail.com>
2021-09-07 13:57:37 +0200

    Prototype of rings

---
+++ code/sbox_stargate/entities/rings_base/RingRing.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Threading.Tasks;
+using Sandbox;
+
+public partial class RingRing : Prop {
+
+	public Rings Parent;
+
+	public bool isUpsideDown = false;
+
+	private bool reachedPos = false;
+	public bool Ready {
+		get {
+			return reachedPos;
+		}
+	}
+	public float desiredPos;
+
+	public override void Spawn() {
+		base.Spawn();
+		Tags.Add( "no_rings_teleport" );
+
+		EnableHitboxes = false;
+		PhysicsEnabled = false;
+		RenderAlpha = 0;
+
+		Transmit = TransmitType.Always;
+		SetModel( "models/gmod4phun/stargate/rings_ancient/ring_ancient.vmdl" );
+	}
+
+	public async Task<bool> MoveUp(int num) {
+		RenderAlpha = 1;
+		reachedPos = false;
+		while (!reachedPos) {
+
+			if (!this.IsValid())
+				break;
+
+			var speed = (desiredPos - LocalPosition.z) / 10;
+			LocalPosition += new Vector3(0, 0, speed);
+			
+			if (Math.Abs(LocalPosition.z - desiredPos) < 5) {
+				reachedPos = true;
+				break;
+			}
+
+			await Task.Delay(1);
+		}
+
+		return true;
+	}
+
+	public async Task<bool> Refract() {
+		reachedPos = false;
+		while (!reachedPos) {
+
+			if (!this.IsValid())
+				break;
+
+			var speed = (Math.Abs((desiredPos - LocalPosition.z)) / 10) + 0.1f;
+			LocalPosition -= new Vector3(0, 0, speed);
+
+			if (LocalPosition.z <= 4) {
+				reachedPos = true;
+				break;
+			}
+
+			await Task.Delay(1);
+		}
+
+		Parent.OnRingReturn();
+
+		Delete();
+
+		return true;
+	}
+
+}
