daf4ae009be15ae1ce62dffa7aed56fa60cf3f57
Mathis Cucci <mat.spyder@gmail.com>
2021-09-15 07:09:50 +0200

    Use new override in Goauld Panel

--- code/sbox_stargate/entities/ring_panel_goauld/RingPanelGoauld.cs
+++ code/sbox_stargate/entities/ring_panel_goauld/RingPanelGoauld.cs
@@ -1,11 +1,11 @@
 using Sandbox;
 
-[Library( "ent_rings_panel_goauld", Title = "Rings Panel (Goa'uld)", Spawnable = true, Group = "Stargate" )]
+[Library( "ent_rings_panel_goauld", Title = "Rings Panel (Goa'uld)", Spawnable = true, Group = "Stargate.Stargate" )]
 public partial class RingPanelGoauld : RingPanel {
 
-	protected override int DialButtonNumber {
-		get => 6;
-	}
+	protected override int DialButtonNumber => 6;
+
+	protected override int AmountOfButtons => 6;
 
 	protected override string[] ButtonsSounds { get; } = {
 		"goauld_button1",
