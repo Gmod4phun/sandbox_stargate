8f8edfa55a635e0d23cf1ffcf7a18980be3fb364
Mathis Cucci <mat.spyder@gmail.com>
2021-09-29 06:19:33 +0200

    Make ring panel buttons not being teleported

--- code/sbox_stargate/entities/ring_panel_base/RingPanelButton.cs
+++ code/sbox_stargate/entities/ring_panel_base/RingPanelButton.cs
@@ -6,22 +6,23 @@ public partial class RingPanelButton : AnimEntity, IUse
 
 	[Net]
 	public string Action { get; set; } = "";
-	
+
 	[Net]
 	public bool On { get; set; } = false;
 
 	public override void Spawn()
 	{
 		base.Spawn();
+		Tags.Add( "no_rings_teleport" );
 		Transmit = TransmitType.Always;
 	}
-	public bool OnUse(Entity ent)
+	public bool OnUse( Entity ent )
 	{
 		RingPanel.TriggerAction( Action );
 		return false;
 	}
 
-	public bool IsUsable(Entity ent)
+	public bool IsUsable( Entity ent )
 	{
 		return true;
 	}
@@ -32,7 +33,7 @@ public partial class RingPanelButton : AnimEntity, IUse
 
 		if ( IsServer ) RingPanel?.Delete();
 	}
-	
+
 	public void ButtonGlowLogic()
 	{
 		SetMaterialGroup( On ? 1 : 0 );
