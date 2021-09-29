1fa0af00bc2721eaff67c19f052823f4c5f0eb79
Mathis Cucci <mat.spyder@gmail.com>
2021-09-29 08:08:38 +0200

    Fix data gatespawner not having priority on root

--- code/sbox_stargate/gatespawner/GateSpawner.cs
+++ code/sbox_stargate/gatespawner/GateSpawner.cs
@@ -3,47 +3,52 @@ using System.Linq;
 using System.Text.Json;
 using Sandbox;
 
-public partial class GateSpawner {
+public partial class GateSpawner
+{
 
 	public static List<Entity> GateSpawnerEntites = new();
 
-	public static void CreateGateSpawner() {
+	public static void CreateGateSpawner()
+	{
 		var fileName = Global.MapName;
 
-		if (!FileSystem.Data.DirectoryExists("gatespawners/"))
-			FileSystem.Data.CreateDirectory("gatespawners/");
+		if ( !FileSystem.Data.DirectoryExists( "gatespawners/" ) )
+			FileSystem.Data.CreateDirectory( "gatespawners/" );
 
 		var model = new GatespawnerModel();
-		foreach (IGateSpawner e in Entity.All.OfType<IGateSpawner>()) {
-			model.Entities.Add(e.ToJson());
+		foreach ( IGateSpawner e in Entity.All.OfType<IGateSpawner>() )
+		{
+			model.Entities.Add( e.ToJson() );
 		}
-		FileSystem.Data.WriteAllText($"gatespawners/{fileName}.json", JsonSerializer.Serialize(model, new JsonSerializerOptions() { WriteIndented = true }));
+		FileSystem.Data.WriteAllText( $"gatespawners/{fileName}.json", JsonSerializer.Serialize( model, new JsonSerializerOptions() { WriteIndented = true } ) );
 	}
 
-	public static void LoadGateSpawner() {
+	public static void LoadGateSpawner()
+	{
 		UnloadGateSpawner(); // unload it before loading, we dont want to have multiple instances of loaded entities
 
 		var filepath = $"{Global.MapName}.json";
 
-		bool isData = FileSystem.Data.FileExists($"gatespawners/{filepath}");
-		bool isRoot = FileSystem.Mounted.FileExists($"sbox_stargate/gatespawner/maps/{filepath}");
+		bool isData = FileSystem.Data.FileExists( $"gatespawners/{filepath}" );
+		bool isRoot = !isData && FileSystem.Mounted.FileExists( $"sbox_stargate/gatespawner/maps/{filepath}" );
 
-		if (!isData && !isRoot)
+		if ( !isData && !isRoot )
 			return;
 
 		// Gatespawners in data folder will always have priority
 		filepath = (isRoot ? "sbox_stargate/gatespawner/maps/" : "gatespawners/") + filepath;
 
-		var file = isRoot ? FileSystem.Mounted.ReadAllText(filepath) : FileSystem.Data.ReadAllText(filepath);
-		var data = JsonSerializer.Deserialize<GatespawnerModel>(file);
+		var file = isRoot ? FileSystem.Mounted.ReadAllText( filepath ) : FileSystem.Data.ReadAllText( filepath );
+		var data = JsonSerializer.Deserialize<GatespawnerModel>( file );
 
-		foreach (JsonElement o in data.Entities) {
-			string entityName = o.GetProperty("EntityName").ToString();
-			Entity e = Library.Create<Entity>(entityName, false);
-			if (e is null || !e.IsValid())
+		foreach ( JsonElement o in data.Entities )
+		{
+			string entityName = o.GetProperty( "EntityName" ).ToString();
+			Entity e = Library.Create<Entity>( entityName, false );
+			if ( e is null || !e.IsValid() )
 				continue;
 
-			(e as IGateSpawner).FromJson(o);
+			(e as IGateSpawner).FromJson( o );
 
 			GateSpawnerEntites.Add( e );
 		}
@@ -51,16 +56,18 @@ public partial class GateSpawner {
 
 	public static void UnloadGateSpawner()
 	{
-		foreach (var ent in GateSpawnerEntites )
+		foreach ( var ent in GateSpawnerEntites )
 		{
-			if (ent.IsValid()) ent.Delete();
+			if ( ent.IsValid() ) ent.Delete();
 		}
 		GateSpawnerEntites.Clear();
 	}
 
-	[ServerCmd("gatespawner")]
-	public static void GateSpawnerCmd(string action) {
-		switch(action) {
+	[ServerCmd( "gatespawner" )]
+	public static void GateSpawnerCmd( string action )
+	{
+		switch ( action )
+		{
 			case "create":
 				CreateGateSpawner();
 				break;
@@ -75,7 +82,8 @@ public partial class GateSpawner {
 
 }
 
-public class GatespawnerModel {
+public class GatespawnerModel
+{
 
 	public string Version { get; set; } = "0.0.1";
 
